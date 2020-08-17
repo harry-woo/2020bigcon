16b95a466be63ef1e3cbdb09d238e7e2db29ca78
Harry Woo <mmmshyal@knou.ac.kr>
2020-08-17 22:51:50 +0900

    0817

---
+++ card.R
@@ -0,0 +1,4 @@
+resident <- read.table("02_CARD/RESIDENT/CARD_SPENDING_RESIDENT.txt", header = TRUE)
+foreigner <- read.table("02_CARD/FOREIGNER/CARD_SPENDING_FOREIGNER.txt", sep = "\t", header = TRUE)
+
+head(foreigner)
---
+++ sns.R
@@ -0,0 +1,1 @@
+sns <- read.csv(file = "03_WISENUT/wisenut.csv", header = TRUE, sep = "\a", fileEncoding = "UTF-8")
