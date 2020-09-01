af148bcc377072753a28e8df0d488926f0854fda
Harry Woo <mmmshyal@knou.ac.kr>
2020-09-01 16:48:55 +0900

    0901

--- skt.R
+++ skt.R
@@ -1,6 +1,12 @@
 skt_age_1902 <- read.csv(file = "01_SKT/FLOW_AGE_201902.csv", header = TRUE, sep = "|", fileEncoding="UTF-8-BOM")
 skt_age_2002 <- read.csv(file = "01_SKT/FLOW_AGE_202002.csv", header = TRUE, sep = "|", fileEncoding="UTF-8-BOM")
 
+skt_age_1903 <- read.csv(file = "01_SKT/FLOW_AGE_201903.csv", header = TRUE, sep = "|", fileEncoding="UTF-8-BOM")
+skt_age_2003 <- read.csv(file = "01_SKT/FLOW_AGE_202003.csv", header = TRUE, sep = "|", fileEncoding="UTF-8-BOM")
+
+skt_age_1904 <- read.csv(file = "01_SKT/FLOW_AGE_201904.csv", header = TRUE, sep = "|", fileEncoding="UTF-8-BOM")
+skt_age_2004 <- read.csv(file = "01_SKT/FLOW_AGE_202004.csv", header = TRUE, sep = "|", fileEncoding="UTF-8-BOM")
+
 skt_age_1905 <- read.csv(file = "01_SKT/FLOW_AGE_201905.csv", header = TRUE, sep = "|", fileEncoding="UTF-8-BOM")
 skt_age_2005 <- read.csv(file = "01_SKT/FLOW_AGE_202005.csv", header = TRUE, sep = "|", fileEncoding="UTF-8-BOM")
 
@@ -32,3 +38,56 @@ skt_age_feb %>%
 
 str(skt_age_feb$STD_YM)
 table(skt_age_feb$STD_YM)
+
+
+data.frame(skt_age_1902 %>% 
+             filter(HDONG_NM == "상계6.7동") %>% 
+             summarise("201902" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_1903 %>% 
+             filter(HDONG_NM == "상계6.7동") %>% 
+             summarise("201903" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_1904 %>% 
+             filter(HDONG_NM == "상계6.7동") %>% 
+             summarise("201904" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_1905 %>% 
+             filter(HDONG_NM == "상계6.7동") %>% 
+             summarise("201905" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_2002 %>% 
+             filter(HDONG_NM == "상계6.7동") %>% 
+             summarise("202002" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_2003 %>% 
+             filter(HDONG_NM == "상계6.7동") %>% 
+             summarise("202003" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_2004 %>% 
+             filter(HDONG_NM == "상계6.7동") %>% 
+             summarise("202004" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_2005 %>% 
+             filter(HDONG_NM == "상계6.7동") %>% 
+             summarise("202005" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           )
+
+data.frame(skt_age_1902 %>% 
+             filter(HDONG_NM == "명동") %>% 
+             summarise("201902" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_1903 %>% 
+             filter(HDONG_NM == "명동") %>% 
+             summarise("201903" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_1904 %>% 
+             filter(HDONG_NM == "명동") %>% 
+             summarise("201904" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_1905 %>% 
+             filter(HDONG_NM == "명동") %>% 
+             summarise("201905" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_2002 %>% 
+             filter(HDONG_NM == "명동") %>% 
+             summarise("202002" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_2003 %>% 
+             filter(HDONG_NM == "명동") %>% 
+             summarise("202003" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_2004 %>% 
+             filter(HDONG_NM == "명동") %>% 
+             summarise("202004" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+           , skt_age_2005 %>% 
+             filter(HDONG_NM == "명동") %>% 
+             summarise("202005" = mean(MAN_FLOW_POP_CNT_6064 + MAN_FLOW_POP_CNT_6569 + MAN_FLOW_POP_CNT_70U))
+)
